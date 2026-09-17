a0973eac0eb3e1da4db1188a97ebc94587646acf
krajcsovicskyler-dotcom <krajcsovics.kyler@student.ccm.edu>
2026-09-17 16:36:34 -0400

    Started work on person class

--- Person.java
+++ Person.java
@@ -1,3 +1,50 @@
+import java.util.Scanner;
 public class Person {
-    
-}
+    private static Scanner input = new Scanner(System.in);
+    private String name = input.nextLine();
+    private int age = input.nextInt();
+    private String email = input.nextLine();
+
+    public String getName()
+    {
+        return name;
+    }//ends getName
+
+    private String setName()
+    {
+        System.out.println("Please enter your name.");
+        Person.name = name;
+        return name;
+    }//ends setName
+
+    public int getAge()
+    {
+        return age;
+    }//engs getAge
+
+    private int setAge()
+    {
+        System.out.println("Please enter your age.");
+        Person.age = age;
+        return age;
+    }//ends setAge
+
+    public String getEmail()
+    {
+        return email;
+    }//ends getEmail
+
+    private String setEmail(String email)
+    {
+        System.out.println("Please eneter your email.");
+        Person.email = email;
+        return email;
+    }//ends setEmail
+
+    public String displayInfo(String n, int a, String e)
+    {
+        String info = "Personal Information\n-----------------\nName: " + n + "\nAge: " + a + "\nEmail: " + e;
+        return info;
+    }//ends displayInfo
+
+}//ends class
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,8 @@
 public class PersonTest {
-    
-}
+    public static void main(String args[])
+    {
+        Person p1 = new Person();
+        Person p2 = new Person();
+        p1.setName();
+    }//ends main
+}//ends class
